1819a2bf5336bda9f40eafa8c50de9ae8afb8a61
Daniel Hyams <daniel.hyams@peopletec.com>
2020-05-04 17:35:00 -0500

    overhaul of tests in reqrep, added relaxed testing.

--- src/NetMQ.Tests/ReqRepTests.cs
+++ src/NetMQ.Tests/ReqRepTests.cs
@@ -1,5 +1,6 @@
 ﻿using NetMQ.Sockets;
 using Xunit;
+using System.Threading;
 
 namespace NetMQ.Tests
 {
@@ -7,89 +8,100 @@ namespace NetMQ.Tests
     {
         public ReqRepTests() => NetMQConfig.Cleanup();
 
+        protected void SimpleReqRepSequence(string address, RequestSocket req, ResponseSocket rep)
+        {
+            var port = rep.BindRandomPort(address);
+            req.Connect(address + ":" + port);
+
+            req.SendFrame("Hi");
+            Assert.Equal(new[] { "Hi" }, rep.ReceiveMultipartStrings());
+            rep.SendFrame("Hi2");
+            Assert.Equal(new[] { "Hi2" }, req.ReceiveMultipartStrings());
+        }
+
+        protected void SendTwoReqsInARow(string address, RequestSocket req, ResponseSocket rep)
+        {
+            var port = rep.BindRandomPort(address);
+            req.Connect(address + ":" + port);
+            req.SendFrame("Hi");
+            rep.SkipFrame();
+        }
+
         [Theory]
         [InlineData("tcp://localhost")]
         [InlineData("tcp://127.0.0.1")]
-        public void SimpleReqRep(string address)
+        public void SimpleReqRepSucceeds(string address)
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
             {
-                var port = rep.BindRandomPort(address);
-                req.Connect(address + ":" + port);
-
-                req.SendFrame("Hi");
-
-                Assert.Equal(new[] { "Hi" }, rep.ReceiveMultipartStrings());
-
-                rep.SendFrame("Hi2");
-
-                Assert.Equal(new[] { "Hi2" }, req.ReceiveMultipartStrings());
+                SimpleReqRepSequence(address, req, rep);
             }
         }
 
         [Theory]
         [InlineData("tcp://localhost")]
         [InlineData("tcp://127.0.0.1")]
-        public void SimpleReqRepWithCorrelation(string address)
+        public void SimpleReqRepWithCorrelationSucceeds(string address)
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
             {
                 req.Options.Correlate = true;
-                //req.Options.Relaxed = true;
-
-                var port = rep.BindRandomPort(address);
-                req.Connect(address + ":" + port);
-
-                req.SendFrame("Hi");
-
-                Assert.Equal(new[] { "Hi" }, rep.ReceiveMultipartStrings());
-
-                rep.SendFrame("Hi2");
-
-                Assert.Equal(new[] { "Hi2" }, req.ReceiveMultipartStrings());
+                SimpleReqRepSequence(address, req, rep);
             }
         }
 
-        [Fact]
-        public void SendingTwoRequestsInARow()
+        [Theory]
+        [InlineData("tcp://localhost")]
+        [InlineData("tcp://127.0.0.1")]
+        public void SimpleReqRepWithRelaxedSucceeds(string address)
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
             {
-                var port = rep.BindRandomPort("tcp://localhost");
-                req.Connect("tcp://localhost:" + port);
+                req.Options.Relaxed = true;
+                SimpleReqRepSequence(address, req, rep);
+                req.SendFrame("Hi2"); // ick that this not what I wanted.
+            }
+        }
 
+
+        [Theory]
+        [InlineData("tcp://localhost")]
+        [InlineData("tcp://127.0.0.1")]
+        public void SendingTwoRequestsInARowFails(string address)
+        {
+            using (var rep = new ResponseSocket())
+            using (var req = new RequestSocket())
+            {
+                var port = rep.BindRandomPort(address);
+                req.Connect(address + ":" + port);
                 req.SendFrame("Hi");
-
                 rep.SkipFrame();
-
                 Assert.Throws<FiniteStateMachineException>(() => req.SendFrame("Hi2"));
             }
         }
 
-        [Fact]
-        public void SendingTwoRequestsInARowWithRelaxed()
+        [Theory]
+        [InlineData("tcp://localhost")]
+        [InlineData("tcp://127.0.0.1")]
+        public void SendingTwoRequestsInARowWithRelaxedSucceeds(string address)
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
             {
                 req.Options.Relaxed = true;
 
-                var port = rep.BindRandomPort("tcp://localhost");
-                req.Connect("tcp://localhost:" + port);
-
+                var port = rep.BindRandomPort(address);
+                req.Connect(address + ":" + port);
                 req.SendFrame("Hi");
-
-                //rep.SkipFrame();
-
-                req.SendFrame("Hi2");
+                rep.SkipFrame();
             }
         }
 
-        [Fact] // DGH would like to test this with and without relaxed = true, correlate = true
-        public void ReceiveBeforeSending()
+        [Fact] 
+        public void ReceiveBeforeSendingFails()
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
@@ -103,7 +115,35 @@ namespace NetMQ.Tests
         }
 
         [Fact]
-        public void SendMessageInResponseBeforeReceiving()
+        public void ReceiveBeforeSendingWithRelaxedFails()
+        {
+            using (var rep = new ResponseSocket())
+            using (var req = new RequestSocket())
+            {
+                req.Options.Relaxed = true;
+                var port = rep.BindRandomPort("tcp://localhost");
+                req.Connect("tcp://localhost:" + port);
+
+                Assert.Throws<FiniteStateMachineException>(() => req.ReceiveFrameBytes());
+            }
+        }
+
+        [Fact]
+        public void ReceiveBeforeSendingWithCorrelateFails()
+        {
+            using (var rep = new ResponseSocket())
+            using (var req = new RequestSocket())
+            {
+                req.Options.Correlate = true;
+                var port = rep.BindRandomPort("tcp://localhost");
+                req.Connect("tcp://localhost:" + port);
+
+                Assert.Throws<FiniteStateMachineException>(() => req.ReceiveFrameBytes());
+            }
+        }
+
+        [Fact]
+        public void SendMessageInResponseBeforeReceivingFails()
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
@@ -115,8 +155,37 @@ namespace NetMQ.Tests
             }
         }
 
+        
+
+        // make sure that a single responder sends messages back to the correct requestors.
         [Fact]
-        public void SendMultipartMessage()
+        public void SingleResponderSendsCorrectMessagesToMultipleRequestors()
+        {
+            using (var rep = new ResponseSocket())
+            using (var req1 = new RequestSocket())
+            using (var req2 = new RequestSocket())
+            {
+                var port = rep.BindRandomPort("tcp://127.0.0.1");
+
+                req1.Connect($"tcp://127.0.0.1:{port}");
+                req2.Connect($"tcp://127.0.0.1:{port}");
+
+                req1.SendFrame("From1");
+                req2.SendFrame("From2");
+
+                rep.SendFrame(rep.ReceiveFrameString());
+                rep.SendFrame(rep.ReceiveFrameString());
+
+                Assert.Equal("From2", req2.ReceiveFrameString());
+                Assert.Equal("From1", req1.ReceiveFrameString());
+
+            }
+        }
+
+       
+
+        [Fact]
+        public void SendMultipartMessageSucceeds()
         {
             using (var rep = new ResponseSocket())
             using (var req = new RequestSocket())
